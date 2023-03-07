ed2c8e6b95067542012f3fc228e3786f4e30e619
joyce <joyce.mbt@gmail.com>
2023-03-07 16:57:46 -0300

    calling the routes in the server file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { Application } from "express";
+import { routes } from "./routes";
 
 const app: Application = express();
 
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running in port 3000"));
