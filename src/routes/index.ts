ab7522ee5d7ca269cff6ea59bef1569a01d50a43
joyce <joyce.mbt@gmail.com>
2023-03-09 18:23:23 -0300

    added the new about route to the main routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { aboutRoutes } from "./aboutRoutes";
 import { userRoutes } from "./userRoutes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/about", aboutRoutes);
 
 export { routes };
